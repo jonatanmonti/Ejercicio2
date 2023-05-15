88b3f50c634971c531e24949a4e56d30045ad581
jonatan monti <jonatan.monti@hotmail.com>
2023-05-14 22:11:10 -0300

    creamos las clases

---
+++ Ejercicio2/TipoDeNafta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio2
+{
+    public enum TipoDeNafta
+    {
+        Normal = 0,
+        Super = 1,
+        premium = 2,
+
+    }
+}
